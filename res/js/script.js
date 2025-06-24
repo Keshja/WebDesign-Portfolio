576f55da687348c7b052339fe5bde57a0944e67a
Keshja <171198431+Keshja@users.noreply.github.com>
2025-06-24 20:39:42 +0200

    attempted to split code. Needs further improvements

--- res/js/script.js
+++ res/js/script.js
@@ -11,3 +11,11 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 } 
+
+
+void function(script) {
+    const { searchParams } = new URL(script.src);
+    fetch(searchParams.get('src')).then(r => r.text()).then(content => {
+        script.outerHTML = content;
+    });
+}(document.currentScript);
